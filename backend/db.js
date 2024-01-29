22395c5742d12e77418d308c624dfabde2f1483a
Krishna Mohan <krishanmohank974@gmail.com>
2024-01-29 23:51:29 +0530

    made it more efficient

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,26 @@ const mongoose  = require("mongoose")
 mongoose.connect("mongodb+srv://admin:1rzy7t5zEfdJ8gHW@cluster0.dkfoi6y.mongodb.net/paytm")
 
 const userSchema = new mongoose.Schema({
-    username : string,
-    password : string,
-    name : string,
+    username : {
+        type : string, 
+        required : true,
+        minLength : 3, 
+        maxLength : 20,
+        unique  :true,
+    },
+    password : {
+        type : string, 
+        required : true, 
+        minLength : 6
+    },
+    fullname : {
+        type: string,
+        required : true,
+        maxLength : 50
+    },
 })
 
-const userModel = mongoose.model("User", userSchema)
+const userModel = mongoose.model("userModel", userSchema)
 
 module.exports = {
     userModel
