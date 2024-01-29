b862b407f1859acde688266e05f3fc56f4acb1c2
Krishna Mohan <krishanmohank974@gmail.com>
2024-01-30 03:08:00 +0530

    adding backend auth routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
+const zod = require("zod");
 const router = express.Router();
 
-router.post("/signup");
+// using zod for input validation 
+ const signupSchema = zod.object({
+    username : zod.string(),
+    passowd : zod.string(),
+    fullname : zod.string(),
+ })
+router.post("/signup" , (req, res) => {
+    const body = req.body ;
+    const {success} = signupSchema.safeParse(req.body);
+    if (!success){
+        return res.json({
+            message : "Email is already taken / incorrect inputs"
+        }).status(411)
+    }
+
+
 router.post("/signin");
 
 module.exports= router;
