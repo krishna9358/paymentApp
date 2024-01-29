c2a2c62cbf6058c0dc8ea22447385253744a7f87
Krishna Mohan <krishanmohank974@gmail.com>
2024-01-30 02:21:00 +0530

    added cors, jwt, created configure.js for storing jwt token

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require ("express")
 const router = require("./routes")
-
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors());
+app.use(express.json());
 app.use ("/api/v1", router);  // /api/v1/router ke andar jana 
+
+app.listen(3000, (req, res) => {
+    console.log("running server on port 3000")
+})
