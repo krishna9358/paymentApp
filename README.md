d6119d95b4b29bdf5d1e65f67678694c073e65a1
Krishna <krishanmohank974@gmail.com>
2024-05-16 16:00:48 +0530

    added routes structure and cors

--- README.md
+++ README.md
@@ -7,7 +7,11 @@ zod : Input validation
 
 
 Structure: 
-1. Frontend
-2. Backend
+1. Frontend - src/app.jsx
+2. Backend - index.js
+ - db.js - mongo db schema
+ - routes/index.js - routes 
 
+*NOTE* : routes/index.js - router = express.Router -> import in index.js -> app.use("/routes redirect to", file name in routes) -> index.js in routes redicrect to another route file using router.use(); 
 
+ - 
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express  = require('express');
+const cors = require('cors');
+const mainRouter = require('./routes/index');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-}
-);
-
+app.use(express.json());
+app.use(cors())
+app.use("/api/v1", mainRouter);
+    
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
